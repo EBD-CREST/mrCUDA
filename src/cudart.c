6ca6d5b45dddd408fbe42296c6263b1f115510e1
Pak Markthub <pak@pak-matsu-lab.(none)>
2014-10-07 13:58:47 +0900

    Finish implementing rCUDA to native migration mini prototype

--- src/cudart.c
+++ src/cudart.c
@@ -16,6 +16,7 @@
 static void *__nvidia_cudart_handle;
 static void *__rcuda_cudart_handle;
 
+/* NVIDIA CUDA */
 static void ** (*__nvidia___cudaRegisterFatBinary)(void *);
 static void CUDARTAPI (*__nvidia___cudaRegisterFunction)(void **, const char *, char *, const char *, int, uint3 *, uint3 *, dim3 *, dim3 *, int *);
 static __host__ __cudart_builtin__ cudaError_t CUDARTAPI (*__nvidia_cudaMalloc)(void **, size_t);
@@ -23,7 +24,9 @@ static __host__ cudaError_t CUDARTAPI (*__nvidia_cudaMemcpy)(void *, const void 
 static __host__ cudaError_t CUDARTAPI (*__nvidia_cudaLaunch)(const void *);
 static __host__ cudaError_t CUDARTAPI (*__nvidia_cudaSetupArgument)(const void *, size_t, size_t);
 static __host__ cudaError_t CUDARTAPI (*__nvidia_cudaConfigureCall)(dim3, dim3, size_t, cudaStream_t);
+static __host__ __cudart_builtin__ cudaError_t CUDARTAPI (*__nvidia_cudaFree)(void *);
 
+/* RCUDA */
 static void ** (*__rcuda___cudaRegisterFatBinary)(void *);
 static void CUDARTAPI (*__rcuda___cudaRegisterFunction)(void **, const char *, char *, const char *, int, uint3 *, uint3 *, dim3 *, dim3 *, int *);
 static __host__ __cudart_builtin__ cudaError_t CUDARTAPI (*__rcuda_cudaMalloc)(void **, size_t);
@@ -31,10 +34,31 @@ static __host__ cudaError_t CUDARTAPI (*__rcuda_cudaMemcpy)(void *, const void *
 static __host__ cudaError_t CUDARTAPI (*__rcuda_cudaLaunch)(const void *);
 static __host__ cudaError_t CUDARTAPI (*__rcuda_cudaSetupArgument)(const void *, size_t, size_t);
 static __host__ cudaError_t CUDARTAPI (*__rcuda_cudaConfigureCall)(dim3, dim3, size_t, cudaStream_t);
+static __host__ __cudart_builtin__ cudaError_t CUDARTAPI (*__rcuda_cudaFree)(void *);
+
+/* Default */
+static void ** (*__default___cudaRegisterFatBinary)(void *);
+static void CUDARTAPI (*__default___cudaRegisterFunction)(void **, const char *, char *, const char *, int, uint3 *, uint3 *, dim3 *, dim3 *, int *);
+static __host__ __cudart_builtin__ cudaError_t CUDARTAPI (*__default_cudaMalloc)(void **, size_t);
+static __host__ cudaError_t CUDARTAPI (*__default_cudaMemcpy)(void *, const void *, size_t, enum cudaMemcpyKind);
+static __host__ cudaError_t CUDARTAPI (*__default_cudaLaunch)(const void *);
+static __host__ cudaError_t CUDARTAPI (*__default_cudaSetupArgument)(const void *, size_t, size_t);
+static __host__ cudaError_t CUDARTAPI (*__default_cudaConfigureCall)(dim3, dim3, size_t, cudaStream_t);
+static __host__ __cudart_builtin__ cudaError_t CUDARTAPI (*__default_cudaFree)(void *);
 
 
 static void **__nvidia_fatCubinHandle;
 static void **__rcuda_fatCubinHandle;
+
+typedef struct __CacheMalloc
+{
+    void *devPtr;
+    size_t size;
+    struct __CacheMalloc *next;
+} __CacheMalloc;
+
+static __CacheMalloc *__first_cachemalloc = NULL;
+static __CacheMalloc *__last_cachemalloc = NULL;
 
 static inline void *__safe_dlsym(void *handle, const char *symbol)
 {
@@ -76,6 +100,7 @@ void init(void)
     __nvidia_cudaLaunch = __safe_dlsym(__nvidia_cudart_handle, "cudaLaunch");
     __nvidia_cudaSetupArgument = __safe_dlsym(__nvidia_cudart_handle, "cudaSetupArgument");
     __nvidia_cudaConfigureCall = __safe_dlsym(__nvidia_cudart_handle, "cudaConfigureCall");
+    __nvidia_cudaFree = __safe_dlsym(__nvidia_cudart_handle, "cudaFree");
 
     __rcuda___cudaRegisterFatBinary = __safe_dlsym(__rcuda_cudart_handle, "__cudaRegisterFatBinary");
     __rcuda___cudaRegisterFunction = __safe_dlsym(__rcuda_cudart_handle, "__cudaRegisterFunction");
@@ -84,6 +109,16 @@ void init(void)
     __rcuda_cudaLaunch = __safe_dlsym(__rcuda_cudart_handle, "cudaLaunch");
     __rcuda_cudaSetupArgument = __safe_dlsym(__rcuda_cudart_handle, "cudaSetupArgument");
     __rcuda_cudaConfigureCall = __safe_dlsym(__rcuda_cudart_handle, "cudaConfigureCall");
+    __rcuda_cudaFree = __safe_dlsym(__rcuda_cudart_handle, "cudaFree");
+
+    __default___cudaRegisterFatBinary = __rcuda___cudaRegisterFatBinary;
+    __default___cudaRegisterFunction = __rcuda___cudaRegisterFunction;
+    __default_cudaMalloc = __rcuda_cudaMalloc;
+    __default_cudaMemcpy = __rcuda_cudaMemcpy;
+    __default_cudaLaunch = __rcuda_cudaLaunch;
+    __default_cudaSetupArgument = __rcuda_cudaSetupArgument;
+    __default_cudaConfigureCall = __rcuda_cudaConfigureCall;
+    __default_cudaFree = __rcuda_cudaFree;
 
     DPRINTF("Exit init\n");
 }
@@ -101,6 +136,61 @@ void fini(void)
         dlclose(__rcuda_cudart_handle);
     }
     DPRINTF("Exit fini\n");
+}
+
+extern void mrcudaSwitchToNative()
+{
+    __CacheMalloc *cachemalloc = __first_cachemalloc;
+    void *devPtr;
+    cudaError_t error;
+    void *dataCache;
+
+    /* Replay cudaMalloc */
+    DPRINTF("Enter mrcudaSwitchToNative\n");
+    while(cachemalloc != NULL)
+    {
+        error = (*__nvidia_cudaMalloc)(&devPtr, cachemalloc->size);
+        if(error != cudaSuccess)
+        {
+            fprintf(stderr, "Cannot replay cudaMalloc.\n");
+            exit(EXIT_FAILURE);
+        }
+        dataCache = malloc(cachemalloc->size);
+        if(dataCache == NULL)
+        {
+            fprintf(stderr, "Cannot allocate dataCache.\n");
+            exit(EXIT_FAILURE);
+        }
+        error = (*__rcuda_cudaMemcpy)(dataCache, cachemalloc->devPtr, cachemalloc->size, cudaMemcpyDeviceToHost);
+        if(error != cudaSuccess)
+        {
+            fprintf(stderr, "Cannot perform __rcuda_cudaMemcpy.\n");
+            exit(EXIT_FAILURE);
+        }
+        error = (*__nvidia_cudaMemcpy)(devPtr, dataCache, cachemalloc->size, cudaMemcpyHostToDevice);
+        if(error != cudaSuccess)
+        {
+            fprintf(stderr, "Cannot perform __nvidia_cudaMemcpy.\n");
+            exit(EXIT_FAILURE);
+        }
+        free(dataCache);
+        __first_cachemalloc = cachemalloc->next;
+        free(cachemalloc);
+        cachemalloc = __first_cachemalloc;
+    }
+    __first_cachemalloc = NULL;
+    __last_cachemalloc = NULL;
+
+    /* Change default function handler */
+    __default___cudaRegisterFatBinary = __nvidia___cudaRegisterFatBinary;
+    __default___cudaRegisterFunction = __nvidia___cudaRegisterFunction;
+    __default_cudaMalloc = __nvidia_cudaMalloc;
+    __default_cudaMemcpy = __nvidia_cudaMemcpy;
+    __default_cudaLaunch = __nvidia_cudaLaunch;
+    __default_cudaSetupArgument = __nvidia_cudaSetupArgument;
+    __default_cudaConfigureCall = __nvidia_cudaConfigureCall;
+    __default_cudaFree = __nvidia_cudaFree;
+    DPRINTF("Exit mrcudaSwitchToNative\n");
 }
 
 void **__cudaRegisterFatBinary(void *fatCubin)
@@ -156,11 +246,34 @@ extern void CUDARTAPI __cudaRegisterFunction(
 
 extern __host__ __cudart_builtin__ cudaError_t CUDARTAPI cudaMalloc(void **devPtr, size_t size)
 {
-    cudaError_t ret;
-    DPRINTF("Enter cudaMalloc\n");
-    (*__nvidia_cudaMalloc)(devPtr, size);
-    ret = (*__rcuda_cudaMalloc)(devPtr, size);
-    DPRINTF("Exit cudaMalloc\n");
+    cudaError_t ret = cudaSuccess;
+    __CacheMalloc *cachemalloc;
+
+    if(devPtr == NULL && size == 0)
+        mrcudaSwitchToNative();
+    else
+    {
+        DPRINTF("Enter cudaMalloc\n");
+        ret = (*__default_cudaMalloc)(devPtr, size);
+        if(__default_cudaMalloc == __rcuda_cudaMalloc)
+        {
+            cachemalloc = malloc(sizeof(__CacheMalloc));
+            if(cachemalloc == NULL)
+            {
+                fprintf(stderr, "Cannot allocate a new __CacheMalloc.\n");
+                exit(EXIT_FAILURE);
+            }
+            cachemalloc->devPtr = *devPtr;
+            cachemalloc->size = size;
+            cachemalloc->next = NULL;
+            if(__first_cachemalloc == NULL)
+                __first_cachemalloc = cachemalloc;
+            if(__last_cachemalloc != NULL)
+                __last_cachemalloc->next = cachemalloc;
+            __last_cachemalloc = cachemalloc;
+        }
+        DPRINTF("Exit cudaMalloc\n");
+    }
     return ret;
 }
 
@@ -168,16 +281,7 @@ extern __host__ cudaError_t CUDARTAPI cudaMemcpy(void *dst, const void *src, siz
 {
     cudaError_t ret;
     DPRINTF("Enter cudaMemcpy\n");
-    (*__nvidia_cudaMemcpy)(dst, src, count, kind);
-    if(kind == cudaMemcpyDeviceToHost)
-    {
-        printf("nvidia: %s\n", (char *)dst);
-    }
-    ret = (*__rcuda_cudaMemcpy)(dst, src, count, kind);
-    if(kind == cudaMemcpyDeviceToHost)
-    {
-        printf("rcuda: %s\n", (char *)dst);
-    }
+    ret = (*__default_cudaMemcpy)(dst, src, count, kind);
     DPRINTF("Exit cudaMemcpy\n");
     return ret;
 }
@@ -186,8 +290,7 @@ extern __host__ cudaError_t CUDARTAPI cudaLaunch(const void *func)
 {
     cudaError_t ret;
     DPRINTF("Enter cudaLaunch\n");
-    (*__nvidia_cudaLaunch)(func);
-    ret = (*__rcuda_cudaLaunch)(func);
+    ret = (*__default_cudaLaunch)(func);
     DPRINTF("Exit cudaLaunch\n");
     return ret;
 }
@@ -196,8 +299,7 @@ extern __host__ cudaError_t CUDARTAPI cudaSetupArgument(const void *arg, size_t 
 {
     cudaError_t ret;
     DPRINTF("Enter cudaSetupArgument\n");
-    (*__nvidia_cudaSetupArgument)(arg, size, offset);
-    ret = (*__rcuda_cudaSetupArgument)(arg, size, offset);
+    ret = (*__default_cudaSetupArgument)(arg, size, offset);
     DPRINTF("Exit cudaSetupArgument\n");
     return ret;
 }
@@ -206,8 +308,16 @@ extern __host__ cudaError_t CUDARTAPI cudaConfigureCall(dim3 gridDim, dim3 block
 {
     cudaError_t ret;
     DPRINTF("Enter cudaConfigureCall\n");
-    (*__nvidia_cudaConfigureCall)(gridDim, blockDim, sharedMem, stream);
-    ret = (*__rcuda_cudaConfigureCall)(gridDim, blockDim, sharedMem, stream);
+    ret = (*__default_cudaConfigureCall)(gridDim, blockDim, sharedMem, stream);
     DPRINTF("Exit cudaConfigureCall\n");
+    return ret;
+}
+
+extern __host__ __cudart_builtin__ cudaError_t CUDARTAPI cudaFree(void *devPtr)
+{
+    cudaError_t ret;
+    DPRINTF("Enter cudaFree\n");
+    ret = (*__default_cudaFree)(devPtr);
+    DPRINTF("Exit cudaFree\n");
     return ret;
 }
---
+++ test/matmul_par.cu
@@ -0,0 +1,166 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <sys/time.h>
+
+#define BS (16)
+#define L (16)
+#define M (16)
+#define N (16)
+
+__global__ void matmul(float *A, float *B, float *C,
+                       int l, int m, int n)
+{
+    int i, j, k;
+    float sum;
+
+    i = blockIdx.y * blockDim.y + threadIdx.y;
+    j = blockIdx.x * blockDim.x + threadIdx.x;
+
+    sum = 0.0;
+    for (k = 0; k < m; k++) {
+        sum += A[i * m + k] * B[k * n + j];
+    }
+    C[i*n+j] = sum;
+}
+
+void matmul_cpu(float *A, float *B, float *C,
+                       int l, int m, int n)
+{
+    int i, j, k;
+    for (i = 0; i < l; i++) {
+        for (j = 0; j < n; j++) {
+            float sum = 0.0;
+            for (k = 0; k < m; k++) {
+                sum += A[i * m + k] * B[k * n + j];
+            }
+            C[i*n+j] = sum;
+        }
+    }
+}
+
+int compare_matrix(float *A, float *B, int l, int n)
+{
+    int i, j;
+    int ret = 0;
+    for (i = 0; i < l; i++) {
+        for (j = 0; j < n; j++) {
+            printf("%f ", A[i * n + j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+    for (i = 0; i < l; i++) {
+        for (j = 0; j < n; j++) {
+            printf("%f ", B[i * n + j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+    for (i = 0; i < l; i++) {
+        for (j = 0; j < n; j++) {
+            if(A[i * n + j] != B[i * n + j])
+                ret = -1;
+        }
+    }
+    return ret;
+}
+
+void alloc_matrix(float **m_h, float **m_d, int h, int w)
+{
+    *m_h = (float *)malloc(sizeof(float) * h * w);
+    cudaMalloc((void **)m_d, sizeof(float) * h * w);
+}
+
+void init_matrix(float *m, int h, int w)
+{
+    int i, j;
+    for (i = 0; i < h; i++)
+        for (j = 0; j < w; j++)
+            m[i * w + j] = (float)(random() % 100);
+}
+
+int check_error(const char *err_msg)
+{
+    cudaError_t err = cudaGetLastError();
+    if (err != cudaSuccess) {
+        fprintf(stderr, "CUDA error: %s: %s.\n",
+                err_msg, cudaGetErrorString(err));
+        return 1;
+    }
+    return 0;
+}
+
+double get_elapsed_time(struct timeval *begin, struct timeval *end)
+{
+    return (end->tv_sec - begin->tv_sec) * 1000
+            + (end->tv_usec - begin->tv_usec) / 1000.0;
+}
+
+int main(int argc, char *argv[])
+{
+    float *Ad, *Bd, *Cd;
+    float *Ah, *Bh, *Ch;
+    struct timeval t1, t2;
+
+    // prepare matrix A
+    alloc_matrix(&Ah, &Ad, L, M);
+    init_matrix(Ah, L, M);
+    cudaMemcpy(Ad, Ah, sizeof(float) * L * M,
+               cudaMemcpyHostToDevice);
+    // do it again for matrix B
+    alloc_matrix(&Bh, &Bd, M, N);
+    init_matrix(Bh, M, N);
+    cudaMemcpy(Bd, Bh, sizeof(float) * M * N,
+               cudaMemcpyHostToDevice);
+    // allocate spaces for matrix C
+    alloc_matrix(&Ch, &Cd, L, N);
+
+    cudaDeviceSynchronize();
+    gettimeofday(&t1, NULL);
+
+    // launch matmul kernel
+    matmul<<<dim3(N / BS, L / BS),
+            dim3(BS, BS)>>>(Ad, Bd, Cd, L, M, N);
+
+    if (check_error("matmul")) {
+        exit(EXIT_FAILURE);
+    }
+
+    cudaDeviceSynchronize();
+    gettimeofday(&t2, NULL);
+    printf("Elapsed time: %f msec\n", get_elapsed_time(&t1, &t2));
+    
+    // obtain the result
+    cudaMemcpy(Ch, Cd, sizeof(float) * L * N, cudaMemcpyDeviceToHost);
+    float *C_cpu = (float *)malloc(sizeof(float) * L * N);
+    matmul_cpu(Ah, Bh, C_cpu, L, M, N);
+
+    if(compare_matrix(Ch, C_cpu, L, N) >= 0)
+        printf("OK\n");
+    else
+        printf("ERRRRR\n");
+
+    /* Switch to native */
+    cudaMalloc(NULL, 0);
+
+    matmul<<<dim3(N / BS, L / BS),
+            dim3(BS, BS)>>>(Ad, Bd, Cd, L, M, N);
+    cudaMemcpy(Ch, Cd, sizeof(float) * L * N, cudaMemcpyDeviceToHost);
+    if(compare_matrix(Ch, C_cpu, L, N) >= 0)
+        printf("OK\n");
+    else
+        printf("ERRRRR\n");
+
+    free(C_cpu);
+
+    cudaFree(Ad);
+    cudaFree(Bd);
+    cudaFree(Cd);
+
+    return 0;
+}
+
+
+
